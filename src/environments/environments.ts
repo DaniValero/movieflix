5074a54bd803d293109600b287026e7b95447d4b
Daniel Valero <114396949+DaniValero@users.noreply.github.com>
2023-10-18 10:20:13 +0200

    directory update

--- src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-export const environments = {
-  baseURL: 'http://localhost:4200',
-  apiToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODNjOWRmNTQxMDZiODNjYzc2OWM3MjgyMmMxMDVhMyIsInN1YiI6IjY1MDJjMGI4ZDdkY2QyMDBjNTM3ZGM1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t1KCgFLGE-AkUVaephGHR_7yZzjB4l2LOxUzgte11cw',
-  backendUrl: 'http://localhost:3000',
-};
